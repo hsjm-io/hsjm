42764c531047893cab89fd5e0d9f89549b1d8ece
Stanley Horwood <stanley@hsjm.io>
2022-03-01 12:42:07 +0100

    added `Nato` component

--- src/components/Nato.ts
+++ src/components/Nato.ts
@@ -1,42 +1,48 @@
-import { defineComponent, h, nextTick, onMounted, PropType, ref, watch } from 'vue-demi'
-import MilSymbol from 'milsymbol'
+import { defineComponent, defineAsyncComponent, h, nextTick, onMounted, PropType, ref, watch } from 'vue-demi'
+import { SymbolOptions } from 'milsymbol'
+import { templateRef } from '@vueuse/core'
 
-export const Nato = defineComponent({
-  props: {
-    as: {
-      type: String as PropType<keyof HTMLElementTagNameMap>,
-      default: 'span'
+export const Nato = defineAsyncComponent(async () => {
+  const { Symbol } = await import('milsymbol')
+  
+  return defineComponent({
+    name: 'Nato',
+    props: {
+      as: { type: String as PropType<keyof HTMLElementTagNameMap>, default: 'span' },
+      symbol: { type: [String, Object] as PropType<string | SymbolOptions>, required: true }
     },
 
-    icon: {
-      type: String,
-      required: true,
-    },
-  },
+    setup: (props) => {
+      const el = templateRef('el')
+      const sidc = ref<string | null>(null)
+      
+      // --- Create and append the icon in the template.
+      const update = () => nextTick().then(() => {
 
-  setup: (props, { slots }) => {
-    const el = ref<HTMLElement | null>(null)
-    
-    const update = async() => {
-      await nextTick()
+        // --- Abort if the element was not rendered.
+        if (!el.value) return
 
-      if (!el.value) return
+        // --- Generate the symbol.
+        const symbol = new Symbol(props.symbol, { size: 24 })
+        sidc.value = symbol.getOptions().sidc ?? null
 
-      const svgData = new MilSymbol.Symbol(props.icon, { size: 24 }).asSVG()
-      const svg = document.createElement('svg')
-      svg.innerHTML = svgData
-      el.value.textContent = ''
-      el.value.appendChild(svg)
+        // --- If successful, append it in the template.
+        const svg = symbol.asDOM() as SVGAElement
+        el.value.textContent = ''
+        el.value.appendChild(svg)
+      })
+
+      // --- Update symbol on init or on `symbol` change.
+      onMounted(update)
+      watch(() => props.symbol, update, { flush: 'post', deep: true })
+
+      // @ts-ignore --- Render the VNode.
+      return () => h(props.as, {
+        ref: 'el',
+        role: 'img',
+        'aria-labelledby': sidc.value,
+        'aria-hidden': 'true'
+      })
     }
-    
-    watch(
-      () => props.icon,
-      update,
-      { flush: 'post' },
-    )
-    
-    onMounted(update)
-
-    return () => h(props.as, slots)
-  }
+  })
 })
