84375dbdad8e2cb71f2d12fe24c1013cdff16cae
Stanley Horwood <stanley@hsjm.io>
2022-03-01 12:42:35 +0100

    updated coomponent resolver for `vite-plugin-component`

--- src/utils/pompauteResolver.ts
+++ src/utils/pompauteResolver.ts
@@ -1,14 +1,19 @@
 import { ComponentResolver } from 'unplugin-vue-components/types'
 
 interface PompauteResolverOptions {
+  prefix?: boolean | string
 }
 
-export const PompauteResolver = (options?: PompauteResolverOptions): ComponentResolver => ({
+export const PompauteResolver = (options = {} as PompauteResolverOptions): ComponentResolver => ({
   type: 'component',
   resolve: (name: string) => {
-    return { 
-      Button: { path: 'pompaute', importName: 'Button' },
-      RouterLink: { path: 'vue-router', importName: 'RouterLink' }
+
+    const prefix = options.prefix || ''
+
+    return {
+      [`${prefix}Button`]: { path: 'pompaute', importName: 'Button' },
+      [`${prefix}Icon`]: { path: 'pompaute', importName: 'Icon' },
+      [`${prefix}Nato`]: { path: 'pompaute', importName: 'Nato' },
     }[name]
   }
 })
