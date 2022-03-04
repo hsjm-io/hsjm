b8d5578c87a78f9b757825f0e559bdddf5804bc8
Stanley Horwood <stanley@hsjm.io>
2022-03-04 23:53:19 +0100

    converted `Nato` into an async component

--- src/components/Nato.ts
+++ src/components/Nato.ts
@@ -1,47 +1,51 @@
-import { defineComponent, h, nextTick, onMounted, PropType, ref, watch } from 'vue-demi'
-import { SymbolOptions, Symbol } from 'milsymbol'
+import { defineAsyncComponent, defineComponent, h, nextTick, onMounted, PropType, ref, watch } from 'vue-demi'
+import { SymbolOptions } from 'milsymbol'
 import { templateRef } from '@vueuse/core'
 
-export const Nato = defineComponent({
-  name: 'Nato',
-  props: {
-    as: { type: String as PropType<keyof HTMLElementTagNameMap>, default: 'span' },
-    symbol: { type: [String, Object] as PropType<string | SymbolOptions>, required: true }
-  },
+export const Nato = defineAsyncComponent(async () => {
+  const MilSymbol = await import('milsymbol')
 
-  setup: (props) => {
-    const el = templateRef('el')
-    const sidc = ref<string | null>(null)
-    
-    // --- Create and append the icon in the template.
-    const update = async () => {
-      await nextTick()
+  return defineComponent({
+    name: 'Nato',
+    props: {
+      as: { type: String as PropType<keyof HTMLElementTagNameMap>, default: 'span' },
+      symbol: { type: [String, Object] as PropType<string | SymbolOptions>, required: true }
+    },
 
-      // --- Abort if the element was not rendered.
-      if (!el.value) return
+    setup: (props) => {
+      const el = templateRef('el')
+      const sidc = ref<string | null>(null)
+      
+      // --- Create and append the icon in the template.
+      const update = async () => {
+        await nextTick()
 
-      // --- Generate the symbol.
-      const symbol = new Symbol(props.symbol, { size: 24 })
-      sidc.value = symbol.getOptions().sidc ?? null
+        // --- Abort if the element was not rendered.
+        if (!el.value) return
 
-      // --- If successful, append it in the template.
-      const svgData = symbol.asSVG()
-      const svg = document.createElement('svg')
-      svg.innerHTML = svgData
-      el.value.textContent = ''
-      el.value.appendChild(svg)
+        // --- Generate the symbol.
+        const symbol = new MilSymbol.Symbol(props.symbol, { size: 24 })
+        sidc.value = symbol.getOptions().sidc ?? null
+
+        // --- If successful, append it in the template.
+        const svgData = symbol.asSVG()
+        const svg = document.createElement('svg')
+        svg.innerHTML = svgData
+        el.value.textContent = ''
+        el.value.appendChild(svg)
+      }
+
+      // --- Update symbol on init or on `symbol` change.
+      onMounted(update)
+      watch(() => props.symbol, update, { flush: 'post', deep: true })
+
+      // @ts-ignore --- Render the VNode.
+      return () => h(props.as, {
+        ref: 'el',
+        role: 'img',
+        'aria-labelledby': sidc.value,
+        'aria-hidden': 'true'
+      })
     }
-
-    // --- Update symbol on init or on `symbol` change.
-    onMounted(update)
-    watch(() => props.symbol, update, { flush: 'post', deep: true })
-
-    // @ts-ignore --- Render the VNode.
-    return () => h(props.as, {
-      ref: 'el',
-      role: 'img',
-      'aria-labelledby': sidc.value,
-      'aria-hidden': 'true'
-    })
-  }
+  })
 })
