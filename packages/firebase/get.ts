784f8d20e57b31ea12387142368e6783156a51dc
Stanley Horwood <stanley@hsjm.io>
2022-03-27 03:51:16 +0200

    feat: improved `get` type inference

---
+++ packages/firebase/get.ts
@@ -0,0 +1,98 @@
+import { Ref, isRef, ref, unref, watch } from 'vue-demi'
+import { MaybeRef, extendRef, isClient, reactify, tryOnScopeDispose } from '@vueuse/core'
+import { DocumentData, FirestoreError, Unsubscribe, getDoc, getDocs, onSnapshot } from 'firebase/firestore'
+import { resolvable } from '@hsjm/core'
+import { QueryFilter, createQuery } from './createQuery'
+import { isDocumentReference, unpeelSnapshot } from './utils'
+
+export interface GetOptions {
+  /** Error handler. */
+  onError?: (error: FirestoreError) => void
+  /** Sync the data using `onSnapshot` method. */
+  onSnapshot?: boolean
+}
+
+// eslint-disable-next-line unicorn/prevent-abbreviations
+export interface RefFirebase<T extends DocumentData | DocumentData[]> extends Ref<T> {
+  ready: Promise<void>
+  loading: boolean
+  update: () => void
+}
+
+// --- Cache register.
+export const _cache: Record<string, any> = {}
+
+// --- Overloads.
+interface Get {
+  <T extends DocumentData>(path: MaybeRef<string>, filter: MaybeRef<string>, initialValue?: MaybeRef<T>, optidons?: GetOptions): RefFirebase<T>
+  <T extends DocumentData>(path: MaybeRef<string>, filter: MaybeRef<QueryFilter>, initialValue?: MaybeRef<T[]>, options?: GetOptions): RefFirebase<T[]>
+  <T extends DocumentData>(path: MaybeRef<string>, filter: MaybeRef<string | QueryFilter>, initialValue?: MaybeRef<T | T[]>, options?: GetOptions): RefFirebase<T | T[]>
+}
+
+/**
+ * Fetch data from firestore and bind it to a `Ref`
+ * @param path Path to the collection
+ * @param filter ID or filter parameters.
+ * @param initialValue Initial value of the returned `Ref`.
+ * @param options Custom parameters of the method.
+ */
+export const get: Get = (path: MaybeRef<string>, filter: MaybeRef<string | QueryFilter>, initialValue?: MaybeRef<DocumentData | DocumentData[]>, options = {} as GetOptions) => {
+  // --- Caching.
+  const cacheId = `${!!options.onSnapshot}:${path}:${JSON.stringify(filter)}`
+  if (cacheId in _cache) return _cache[cacheId]
+
+  // --- Init local variables.
+  let update: () => void
+  const { promise, resolve, pending, reset } = resolvable<void>()
+  const query = reactify(createQuery)(path, filter)
+
+  // --- Init `data` ref.
+  if (!initialValue) initialValue = typeof unref(filter) === 'string' ? {} : []
+  const data: any = isRef(initialValue) ? initialValue : ref(initialValue)
+
+  // --- Update wraps `onSnapshot`.
+  if (options.onSnapshot && isClient) {
+    let unsubscribe: Unsubscribe
+    update = () => {
+      // --- Reset promise & unsubscribe if possible.
+      reset()
+      if (unsubscribe) unsubscribe()
+
+      // --- Subscribe to data.
+      unsubscribe = onSnapshot(
+        query.value as any,
+        (snapshot: any) => { data.value = unpeelSnapshot(snapshot); resolve() },
+        options.onError,
+      )
+    }
+
+    // --- Unsubscribe and clear cache on scope dispose.
+    tryOnScopeDispose(() => {
+      if (unsubscribe) unsubscribe()
+      if (_cache[cacheId]) delete _cache[cacheId]
+    })
+  }
+
+  // --- Update wraps `getDoc(s)`.
+  else {
+    update = () => {
+      // --- Reset promise.
+      reset()
+
+      // --- Get data from firestore.
+      const getPromise = isDocumentReference(query.value)
+        ? getDoc(query.value as any).then(unpeelSnapshot)
+        : getDocs(query.value).then(unpeelSnapshot)
+
+      // --- Set date on resolve.
+      getPromise.then((_data) => { data.value = _data; resolve() })
+    }
+  }
+
+  // --- Start `filter` watcher.
+  watch(query, update, { immediate: true })
+
+  // --- Return readonly data ref.
+  extendRef(data, { ready: promise, loading: pending, update })
+  return (_cache[cacheId] = data)
+}
---
+++ packages/firebase/utils.ts
@@ -0,0 +1,33 @@
+import { DocumentData, DocumentReference, DocumentSnapshot, QuerySnapshot } from 'firebase/firestore'
+
+/**
+ * Check if value is of type `DocumentSnapshot`
+ * @param value Value to check.
+ */
+export const isDocumentReference = (value: any): value is DocumentReference => value
+  && typeof value.id === 'string'
+  && value.type === 'document'
+
+/**
+ * Check if value is of type `DocumentSnapshot`
+ * @param value Value to check.
+ */
+export const isDocumentSnapshot = (value: any): value is DocumentSnapshot => value
+  && typeof value.id === 'string'
+  && value.ref.type === 'document'
+
+/**
+ * Extract data from a snapshot of any type.
+ * @param snapshot Snapshot to extract from.
+ */
+export interface UnpeelSnapshot {
+  <T extends DocumentData>(snapshot: QuerySnapshot<T>): T[]
+  <T extends DocumentData>(snapshot: DocumentSnapshot<T>): T
+  <T extends DocumentData>(snapshot: DocumentSnapshot<T> | QuerySnapshot<T>): T | T[]
+}
+
+export const unpeelSnapshot: UnpeelSnapshot = (snapshot: DocumentSnapshot | QuerySnapshot): any => (
+  isDocumentSnapshot(snapshot)
+    ? { id: snapshot.id, ...snapshot.data() }
+    : snapshot.docs.map(x => ({ id: x.id, ...x.data() }))
+)
