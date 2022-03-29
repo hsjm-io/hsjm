af5205c55ec37abc4cd0a233830a33b4c5adc37a
Stanley Horwood <stanley@hsjm.io>
2022-03-29 22:09:06 +0200

    feat: introduced new utills

--- packages/core/utils.ts
+++ packages/core/utils.ts
@@ -1,2 +1,17 @@
 export const clamp = (n: number, min: number, max: number) =>
   Math.min(Math.max(n, min), max)
+
+export const uniq = <T>(array: T[]) =>
+  [...new Set(array)]
+
+export const isNil = (value: any): value is undefined | null =>
+  value !== null && value !== undefined
+
+export const isNotNil = <T>(value: T): value is Exclude<T, undefined | null> =>
+  value !== null && value !== undefined
+
+export const compact = <T>(array: T[]) =>
+  array.filter(isNotNil)
+
+export const compactUniq = <T>(array: T[]) =>
+  uniq(compact(array))
