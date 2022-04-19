08d0ac0abee0f71f28671a94d2dd56251a97d567
Stanley Horwood <stanley@hsjm.io>
2022-04-19 13:20:29 +0200

    feat(unocss-preset): introduced `nord` palette

--- packages/unocss-preset/theme.ts
+++ packages/unocss-preset/theme.ts
@@ -1,8 +1,30 @@
 import { Theme } from '@unocss/preset-mini'
 import { brands } from './constants'
 
-const colorsBrand = Object.entries(brands)
-  .map(([key, brand]) => [key, `#${brand.colors[0]}`])
+export const colorsBrand = Object.fromEntries(Object.entries(brands)
+  .map(([key, brand]) => [key, `#${brand.colors[0]}`]),
+)
+
+export const colorsNord = {
+  nord: {
+    0: '#2E3440',
+    1: '#3B4252',
+    2: '#434C5E',
+    3: '#4C566A',
+    4: '#D8DEE9',
+    5: '#E5E9F0',
+    6: '#ECEFF4',
+    7: '#8FBCBB',
+    8: '#88C0D0',
+    9: '#81A1C1',
+    10: '#5E81AC',
+    11: '#BF616A',
+    12: '#D08770',
+    13: '#EBCB8B',
+    14: '#A3BE8C',
+    15: '#B48EAD',
+  },
+}
 
 export const theme: Theme = {
   easing: {
@@ -10,6 +32,7 @@ export const theme: Theme = {
   },
 
   colors: {
-    ...Object.fromEntries(colorsBrand),
+    ...colorsNord,
+    ...colorsBrand,
   },
 }
