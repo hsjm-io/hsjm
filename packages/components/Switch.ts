22106f5c181df76aa354132f5cce78a43feb0271
Stanley Horwood <stanley@hsjm.io>
2022-04-19 22:28:35 +0200

    feat(components): introduced `Switch` component

---
+++ packages/components/Switch.ts
@@ -0,0 +1,29 @@
+/* eslint-disable unicorn/no-null */
+import { useVModel } from '@vueuse/core'
+import { PropType, defineComponent, h, toRefs } from 'vue-demi'
+import { useSwitch } from '@hsjm/core'
+
+export const Switch = defineComponent({
+  name: 'Switch',
+
+  props: {
+    as: {
+      type: String as PropType<keyof HTMLElementTagNameMap>,
+      default: 'div',
+    },
+    value: { required: true },
+    modelValue: null,
+    disabled: Boolean,
+    readonly: Boolean,
+    loading: Boolean,
+    unique: Boolean,
+    classActive: { type: String, default: '' },
+  },
+
+  setup: (properties, { slots, emit }) => {
+    const { value } = toRefs(properties)
+    const model = useVModel(properties, 'modelValue', emit, { passive: true })
+    const { attributes } = useSwitch(model, value, properties)
+    return () => h(properties.as, attributes, slots)
+  },
+})
