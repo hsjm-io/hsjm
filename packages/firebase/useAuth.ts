bd97b0ec2b2137f05e94f4f8230c5d69b3dda6c2
Stanley Horwood <stanley@hsjm.io>
2022-04-29 14:09:53 +0200

    feat: better app instance caching + auth persistance behavior

--- packages/firebase/useAuth.ts
+++ packages/firebase/useAuth.ts
@@ -1,19 +1,20 @@
-import { createSharedComposable, tryOnMounted } from '@vueuse/shared'
-/* eslint-disable @typescript-eslint/no-var-requires */
+import { resolvable } from '@hsjm/shared'
+import { createUnrefFn } from '@vueuse/core'
+import { tryOnScopeDispose } from '@vueuse/shared'
 import {
   ActionCodeSettings,
   AuthError,
   AuthProvider,
   ConfirmationResult,
+  CustomParameters,
   GithubAuthProvider,
   GoogleAuthProvider,
+  OAuthProvider,
   UserCredential,
   browserPopupRedirectResolver,
   createUserWithEmailAndPassword,
-  getAuth,
   getRedirectResult,
   onAuthStateChanged,
-  sendEmailVerification,
   signInAnonymously,
   signInWithEmailAndPassword,
   signInWithPhoneNumber,
@@ -21,7 +22,6 @@ import {
   signInWithRedirect,
   signOut,
 } from 'firebase/auth'
-import { createUnrefFn } from '@vueuse/core'
 import { ref } from 'vue-demi'
 import { useFirebase } from './useFirebase'
 import { useRecaptcha } from './useRecaptcha'
@@ -29,29 +29,28 @@ import { useRecaptcha } from './useRecaptcha'
 export interface UseAuthOptions extends Partial<ActionCodeSettings> {
   onError?: (error: Error) => void
   onSuccess?: (userCredential: UserCredential) => void
-  useEmailVerification?: boolean
-  useLocalStorage?: boolean
 }
 
-export const useAuth = createSharedComposable((options = {} as UseAuthOptions) => {
+export interface LoginWithProviderOptions extends CustomParameters {
+  scopes: string
+  type: 'popup' | 'redirect'
+}
+
+export const useAuth = (options = {} as UseAuthOptions) => {
   // --- Initialize variables.
   const error = ref<AuthError>()
   const confirmationResult = ref<ConfirmationResult>()
-  const app = useFirebase()
   const recatcha = useRecaptcha()
-  const auth = getAuth(app)
-  auth.useDeviceLanguage()
+  const { auth } = useFirebase()
 
   // --- Extract options.
-  const {
-    onError,
-    onSuccess,
-    useEmailVerification,
-  } = options
+  const { onError, onSuccess } = options
 
-  // --- Restore user.
+  // --- Restore & watch user.
   const user = ref(auth.currentUser)
-  const userId = ref(auth.currentUser?.uid)
+  const ready = resolvable()
+  const unsubscribe = onAuthStateChanged(auth, (_user) => { user.value = _user; ready.resolve() }, onError)
+  tryOnScopeDispose(unsubscribe)
 
   // --- Extend `onError` hook.
   const _onError = (_error: AuthError) => {
@@ -59,82 +58,57 @@ export const useAuth = createSharedComposable((options = {} as UseAuthOptions) =
     if (onError) onError(_error)
   }
 
-  // --- Store auth instance.
-  onAuthStateChanged(auth, (_user) => {
-    user.value = _user
-    userId.value = _user?.uid
-  })
+  // --- Anon auth.
+  const loginAnonymously = () => signInAnonymously(auth).then(onSuccess).catch(_onError)
 
-  tryOnMounted(async() => {
-    await getRedirectResult(auth, browserPopupRedirectResolver)
-      .then((result) => {
-        if (result?.providerId === 'google') GoogleAuthProvider.credentialFromResult(result)
-        if (result?.providerId === 'github') GithubAuthProvider.credentialFromResult(result)
-        if (onSuccess) onSuccess(result)
-      })
-      .catch(_onError)
-  })
+  // --- Phone auth.
+  const loginWithPhone = (phoneNumber: string) => signInWithPhoneNumber(auth, phoneNumber, <any>recatcha).then((data) => { confirmationResult.value = data }).catch(_onError)
+  const confirmCode = (smsCode: string) => confirmationResult.value?.confirm(smsCode).then(onSuccess).catch(_onError)
 
-  const loginAnonymously = () => signInAnonymously(auth)
-    .then(onSuccess)
-    .catch(_onError)
+  // --- Email auth.
+  const registerWithEmail = (email: string, password: string) => createUserWithEmailAndPassword(auth, email, password).then(onSuccess).catch(_onError)
+  const loginWithEmail = (email: string, password: string) => signInWithEmailAndPassword(auth, email, password).then(onSuccess).catch(_onError)
 
-  const loginWithProvider = (provider: AuthProvider | string, popup?: boolean) => {
+  // --- Oauth.
+  const loginFromRedirectResult = () => getRedirectResult(auth).then(result => result && onSuccess && onSuccess(result)).catch(_onError)
+  const loginWithProvider = (provider: AuthProvider | string, options = {} as LoginWithProviderOptions) => {
+    const { scopes, type = 'redirect', ...customParameters } = options
+
     // --- Get provider.
     let _provider = provider
     if (_provider === 'google') _provider = new GoogleAuthProvider()
     if (_provider === 'github') _provider = new GithubAuthProvider()
-    if (typeof _provider === 'string') return _onError(new Error('Invalid auth provider') as AuthError)
 
-    return (popup ? signInWithPopup : signInWithRedirect)(auth, _provider, browserPopupRedirectResolver)
-      .then((result) => {
-        if (provider === 'google') GoogleAuthProvider.credentialFromResult(result)
-        if (provider === 'github') GithubAuthProvider.credentialFromResult(result)
-        if (onSuccess) onSuccess(result)
-      })
+    // @ts-expect-error: Type
+    if (_provider.setCustomParameters && customParameters) (<OAuthProvider>_provider).setCustomParameters(customParameters)
+    // @ts-expect-error: Type
+    if (_provider.addScope && scopes) scopes.split(/[\s']+/).forEach(scope => (<OAuthProvider>_provider).addScope(scope))
+
+    // --- If invalid, throw error.
+    if (typeof _provider === 'string')
+      return _onError(new Error('Invalid auth provider') as AuthError)
+
+    // --- Start.
+    return (type === 'popup' ? signInWithPopup : signInWithRedirect)(auth, _provider, browserPopupRedirectResolver)
+      .then(onSuccess)
       .catch(_onError)
   }
 
-  const loginWithEmail = (email: string, password: string) =>
-    signInWithEmailAndPassword(auth, email, password)
-      .then(onSuccess)
-      .catch(_onError)
-
-  const loginWithPhone = (phoneNumber: string) =>
-    signInWithPhoneNumber(auth, phoneNumber, <any>recatcha)
-      .then((data) => { confirmationResult.value = data })
-      .catch(_onError)
-
-  const confirmCode = (smsCode: string) =>
-    confirmationResult.value?.confirm(smsCode)
-      .then(onSuccess)
-      .catch(_onError)
-
-  const registerWithEmail = (email: string, password: string) =>
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(async(data) => {
-        if (useEmailVerification) await sendEmailVerification(data.user, options as any)
-        return data
-      })
-      .then(onSuccess)
-      .catch(_onError)
-
-  const logout = () =>
-    signOut(auth)
-      .then(<any>onSuccess)
-      .catch(_onError)
+  // --- Yeet-out.
+  const logout = () => signOut(auth).then(<any>onSuccess).catch(_onError)
 
   return {
     auth,
     error,
     user,
-    userId,
+    ready: ready.promise,
     loginAnonymously,
-    loginWithProvider,
-    loginWithEmail: createUnrefFn(loginWithEmail),
     loginWithPhone: createUnrefFn(loginWithPhone),
     confirmCode: createUnrefFn(confirmCode),
     registerWithEmail: createUnrefFn(registerWithEmail),
+    loginWithEmail: createUnrefFn(loginWithEmail),
+    loginFromRedirectResult,
+    loginWithProvider: createUnrefFn(loginWithProvider),
     logout,
   }
-})
+}
--- packages/firebase/useFirebase.ts
+++ packages/firebase/useFirebase.ts
@@ -1,28 +1,37 @@
 
 import { FirebaseOptions, getApp, initializeApp } from 'firebase/app'
-import { FirestoreSettings, connectFirestoreEmulator, initializeFirestore } from 'firebase/firestore'
+import { FirestoreSettings, connectFirestoreEmulator, getFirestore, initializeFirestore } from 'firebase/firestore'
 import { AppCheckOptions, ReCaptchaEnterpriseProvider, ReCaptchaV3Provider, initializeAppCheck } from 'firebase/app-check'
 import { connectFunctionsEmulator, getFunctions } from 'firebase/functions'
 import { connectDatabaseEmulator, getDatabase } from 'firebase/database'
 import { connectStorageEmulator, getStorage } from 'firebase/storage'
-import { Dependencies, connectAuthEmulator, initializeAuth } from 'firebase/auth'
+import { Persistence, PopupRedirectResolver, browserLocalPersistence, browserPopupRedirectResolver, browserSessionPersistence, connectAuthEmulator, getAuth, inMemoryPersistence, indexedDBLocalPersistence, initializeAuth, useDeviceLanguage } from 'firebase/auth'
 import { createSharedComposable } from '@vueuse/shared'
 import { isDevelopment } from '@hsjm/shared'
 
 interface UseFirebaseOptions extends
   FirebaseOptions,
-  FirestoreSettings,
-  Dependencies {
+  FirestoreSettings {
+  /** Local application instance identifier. */
+  name?: string
+
+  // --- App Check
+  /** If set to true, enables automatic background refresh of App Check token. */
+  isTokenAutoRefreshEnabled?: AppCheckOptions['isTokenAutoRefreshEnabled']
+  /** A reCAPTCHA V3 provider, reCAPTCHA Enterprise provider, or custom provider. */
+  appCheckDebugToken?: string | boolean
   /** Key used to create a `ReCaptchaV3Provider` instance. */
   reCaptchaV3ProviderKey?: string
   /** Key used to create a `ReCaptchaEnterpriseProvider` instance. */
   reCaptchaEnterpriseProviderKey?: string
-  /** If set to true, enables automatic background refresh of App Check token. */
-  isTokenAutoRefreshEnabled?: AppCheckOptions['isTokenAutoRefreshEnabled']
-  /** A reCAPTCHA V3 provider, reCAPTCHA Enterprise provider, or custom provider. */
-  attestationProvider?: AppCheckOptions['provider']
-  /** A reCAPTCHA V3 provider, reCAPTCHA Enterprise provider, or custom provider. */
-  appCheckDebugToken?: string | boolean
+
+  // --- Auth.
+  /** Auth persistence mecanism */
+  persistence?: Array<'local' | 'session' | 'memory' | 'indexedDb' | Persistence>
+  /** Auth popup/redirect resolver */
+  popupRedirectResolver?: PopupRedirectResolver
+
+  // --- Emulator.
   /** Emulator port. */
   emulatorHost?: string
   /** Auth emulator port. */
@@ -42,42 +51,72 @@ interface UseFirebaseOptions extends
  * Get the default one if it already exists.
  * @param options Options to configure the app's services.
  */
-export const useFirebase = createSharedComposable((options?: UseFirebaseOptions) => {
+export const useFirebase = createSharedComposable((options?: UseFirebaseOptions | string) => {
   // --- Get app if already initialized.
-  if (!options) return getApp()
+  if (!options || typeof options === 'string') {
+    const app = getApp(options)
+    return {
+      app,
+      auth: getAuth(app),
+      firestore: getFirestore(app),
+      storage: getStorage(app),
+      functions: getFunctions(app),
+      database: getDatabase(app),
+    }
+  }
+
+  // --- Destructure options.
+  const {
+    name,
+    emulatorHost = 'localhost',
+    emulatorAuthPort,
+    emulatorFunctionsPort,
+    emulatorFirestorePort,
+    emulatorStoragePort,
+    emulatorDatabasePort,
+    appCheckDebugToken,
+    reCaptchaV3ProviderKey,
+    reCaptchaEnterpriseProviderKey,
+    isTokenAutoRefreshEnabled,
+    popupRedirectResolver = browserPopupRedirectResolver,
+    persistence = ['local', 'session', 'memory'],
+  } = options
+
+  // --- Prepare auth persistence.
+  const _persistence = persistence.map((x) => {
+    if (typeof x !== 'string') return x
+    if (x === 'local') return browserLocalPersistence
+    if (x === 'session') return browserSessionPersistence
+    if (x === 'indexedDb') return indexedDBLocalPersistence
+    return inMemoryPersistence
+  })
 
   // --- Initialize app.
-  const app = initializeApp(options)
-  const auth = initializeAuth(app, options)
+  const app = initializeApp(options, name)
+  const auth = initializeAuth(app, { popupRedirectResolver, persistence: _persistence })
   const firestore = initializeFirestore(app, options)
+  const storage = getStorage(app)
+  const functions = getFunctions(app)
+  const database = getDatabase(app)
+  useDeviceLanguage(auth)
 
   // --- If available and in devmode, connect to emulator instances.
   if (isDevelopment) {
-    const {
-      emulatorHost = 'localhost',
-      emulatorAuthPort,
-      emulatorFunctionsPort,
-      emulatorFirestorePort,
-      emulatorStoragePort,
-      emulatorDatabasePort,
-      appCheckDebugToken,
-    } = options
     // @ts-expect-error: Missing `self` definition.
     if (appCheckDebugToken) self.FIREBASE_APPCHECK_DEBUG_TOKEN = appCheckDebugToken
     if (emulatorAuthPort) connectAuthEmulator(auth, `http://${emulatorHost}:${emulatorAuthPort}`)
-    if (emulatorStoragePort) connectStorageEmulator(getStorage(app), emulatorHost, emulatorStoragePort)
+    if (emulatorStoragePort) connectStorageEmulator(storage, emulatorHost, emulatorStoragePort)
     if (emulatorFirestorePort) connectFirestoreEmulator(firestore, emulatorHost, emulatorFirestorePort)
-    if (emulatorFunctionsPort) connectFunctionsEmulator(getFunctions(app), emulatorHost, emulatorFunctionsPort)
-    if (emulatorDatabasePort) connectDatabaseEmulator(getDatabase(app), emulatorHost, emulatorDatabasePort)
+    if (emulatorFunctionsPort) connectFunctionsEmulator(functions, emulatorHost, emulatorFunctionsPort)
+    if (emulatorDatabasePort) connectDatabaseEmulator(database, emulatorHost, emulatorDatabasePort)
   }
 
   // --- Register App Checker.
-  let { attestationProvider } = options
-  const { reCaptchaV3ProviderKey, reCaptchaEnterpriseProviderKey, isTokenAutoRefreshEnabled } = options
-  if (!attestationProvider && reCaptchaV3ProviderKey) attestationProvider = new ReCaptchaV3Provider(reCaptchaV3ProviderKey)
-  if (!attestationProvider && reCaptchaEnterpriseProviderKey) attestationProvider = new ReCaptchaEnterpriseProvider(reCaptchaEnterpriseProviderKey)
+  let attestationProvider: any
+  if (reCaptchaV3ProviderKey) attestationProvider = new ReCaptchaV3Provider(reCaptchaV3ProviderKey)
+  if (reCaptchaEnterpriseProviderKey) attestationProvider = new ReCaptchaEnterpriseProvider(reCaptchaEnterpriseProviderKey)
   if (attestationProvider) initializeAppCheck(app, { isTokenAutoRefreshEnabled, provider: attestationProvider })
 
   // --- Return instances.
-  return app
+  return { app, auth, firestore, storage, functions, database }
 })
