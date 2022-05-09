710f67ead8ea0314747d52d631da87de6198eef1
Stanley Horwood <stanley@hsjm.io>
2022-05-09 21:08:07 +0200

    feat(fireworks): split server and client side code into modules

--- packages/fireworks/modules/hierarchy.ts
+++
@@ -1,8 +0,0 @@
-import { Data } from './data'
-
-export interface Hierarchy<T = Data> {
-  parentId?: string
-  childrenIds?: string
-  readonly parent?: T
-  readonly childrens?: T[]
-}
--- packages/fireworks/modules/history.ts
+++
@@ -1,5 +0,0 @@
-import { Data } from './data'
-
-export interface History<T = Data> {
-  readonly history?: T[]
-}
