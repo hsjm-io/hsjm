3b99b031bafd9365559e1b489554ef8029590f0a
Stanley Horwood <stanley@hsjm.io>
2022-05-12 07:20:16 +0200

    fix: added `cwd` property for pm2 instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,9 @@ const glob = require('fast-glob').sync
 const packageDirs = glob('./packages/*', { onlyDirectories: true })
 
 module.exports = {
-  apps : packageDirs.map(x => ({
-    name: x.replace('./packages/', 'hsjm-'),
-    script: `pnpm -C ${x} dev`
+  apps : packageDirs.map(packageDir => ({
+    name: packageDir.replace('./packages/', 'hsjm-'),
+    cwd: packageDir,
+    script: 'pnpm dev',
   }))
 }
