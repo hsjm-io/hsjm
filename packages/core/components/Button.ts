7dc00d4c5d3a1a5487de3eaf6927d294cecaa726
Stanley Horwood <stanley@hsjm.io>
2022-05-20 06:48:21 +0200

    feat(core): removed `useButton` and `useSwitch`

--- packages/core/components/Button.ts
+++ packages/core/components/Button.ts
@@ -1,27 +1,115 @@
-import { PropType, defineComponent, h } from 'vue-demi'
+/* eslint-disable unicorn/prevent-abbreviations */
+/* eslint-disable unicorn/no-null */
+import { useVModel } from '@vueuse/core'
+import { PropType, computed, defineComponent, h, resolveComponent, toRefs } from 'vue-demi'
 import { RouteLocationRaw } from 'vue-router'
-import { useButton } from '../composables'
+import { debounce, noop, throttle } from '@hsjm/shared'
 
 export const Button = defineComponent({
   name: 'Button',
-
   props: {
-    as: {
-      type: String as PropType<keyof HTMLElementTagNameMap>,
-      default: 'button',
-    },
+    as: String as PropType<keyof HTMLElementTagNameMap>,
+
+    // --- State.
     disabled: Boolean,
     readonly: Boolean,
     loading: Boolean,
+    error: Object as PropType<any>,
+
+    // --- Routing.
     to: [String, Object] as PropType<RouteLocationRaw | string>,
-    classActive: { type: String, default: '' },
-    classActiveExact: { type: String, default: '' },
     replace: Boolean,
     newtab: Boolean,
-  },
 
-  setup: (properties, { slots }) => {
-    const { attributes, type } = useButton(properties)
-    return () => h(type as any, attributes, slots)
+    // --- Classes.
+    classActive: Object as PropType<any>,
+    classActiveExact: Object as PropType<any>,
+
+    // --- Events.
+    onClick: Function,
+    debounce: Number,
+    throttle: Number,
+  },
+  setup: (properties, { slots, attrs }) => {
+    // --- Destructure props.
+    const {
+      as,
+      to,
+      newtab,
+      classActive,
+      classActiveExact,
+      onClick,
+      throttle: throttleDelay,
+      debounce: debounceDelay,
+    } = toRefs(properties)
+
+    // --- Compute states variables.
+    const modelDisabled = useVModel(properties, 'disabled', undefined, { passive: true })
+    const modelReadonly = useVModel(properties, 'readonly', undefined, { passive: true })
+    const modelLoading = useVModel(properties, 'loading', undefined, { passive: true })
+    const modelError = useVModel(properties, 'error', undefined, { passive: true })
+
+    // --- Compute routing variables.
+    const isLink = computed<boolean>(() => to.value === true)
+    const isExternalLink = computed(() => isLink.value && typeof to.value === 'string' && !to.value?.startsWith('/'))
+    const isInternalLink = computed(() => isLink.value && !isExternalLink.value)
+    const linkTo = computed(() => (isLink.value && isInternalLink.value && !newtab.value ? to.value : undefined))
+    const linkTarget = computed(() => (isLink.value && newtab.value ? '_blank' : attrs.target as string))
+    const linkRel = computed(() => (isExternalLink.value && newtab.value ? 'noreferrer' : attrs.rel as string))
+    const linkActiveClass = computed(() => (isInternalLink.value ? classActive.value : undefined))
+    const linkExactActiveClass = computed(() => (isInternalLink.value ? classActiveExact.value : undefined))
+
+    // --- Compute component type.
+    const is = computed(() => {
+      if (as.value) return as.value
+      if (isInternalLink.value) return resolveComponent('RouterLink')
+      if (isExternalLink.value) return 'a'
+      return 'button'
+    })
+
+    // --- Wrap `onClick`.
+    const onClickWrapped = computed(() => {
+      // --- Make sure `onClick` is a function.
+      const onClickValue = onClick.value
+      if (typeof onClickValue !== 'function') return noop
+
+      // --- Wrap function to trash loading state & catch error.
+      const callback = () => {
+        const result = onClickValue()
+        if (result instanceof Promise) {
+          modelLoading.value = true
+          result
+            .catch(error => modelError.value = error)
+            .finally(() => modelLoading.value = false)
+        }
+      }
+
+      // --- Return wrapped function.
+      if (throttleDelay.value) return throttle(callback, throttleDelay.value)
+      if (debounceDelay.value) return debounce(callback, debounceDelay.value)
+      return callback
+    })
+
+    // --- Return virtual DOM node.
+    return () => h(is, {
+      // --- State.
+      'disabled': modelDisabled.value ?? null,
+      'readonly': modelReadonly.value ?? null,
+
+      // --- Accessibility.
+      'aria-disabled': modelDisabled.value ?? null,
+      'aria-readonly': modelReadonly.value ?? null,
+      'aria-busy': modelLoading.value ?? null,
+
+      // --- Routing.
+      'to': linkTo,
+      'target': linkTarget,
+      'rel': linkRel,
+      'active-class': linkActiveClass,
+      'exact-active-class': linkExactActiveClass,
+
+      // --- Events.
+      'onClick': onClickWrapped,
+    }, slots)
   },
 })
--- packages/core/components/Switch.ts
+++ packages/core/components/Switch.ts
@@ -1,7 +1,6 @@
 /* eslint-disable unicorn/no-null */
 import { useVModel } from '@vueuse/core'
-import { PropType, defineComponent, h, toRefs } from 'vue-demi'
-import { useSwitch } from '../composables'
+import { PropType, computed, defineComponent, h, reactive, toRefs } from 'vue-demi'
 
 export const Switch = defineComponent({
   name: 'Switch',
@@ -9,21 +8,115 @@ export const Switch = defineComponent({
   props: {
     as: {
       type: String as PropType<keyof HTMLElementTagNameMap>,
-      default: 'div',
+      default: 'button',
     },
-    value: { required: true },
-    modelValue: null,
+
+    // --- State.
     disabled: Boolean,
     readonly: Boolean,
     loading: Boolean,
-    unique: Boolean,
-    classActive: { type: String, default: '' },
+
+    // --- Input.
+    modelValue: {
+      type: [Boolean, Number, String, Array],
+      default: false,
+    },
+    value: {
+      type: [Boolean, Number, String],
+      default: true,
+    },
+    multiple: Boolean,
+
+    // --- Classes.
+    classActive: String,
+
+    // --- Events.
+    onClick: Function,
   },
 
-  setup: (properties, { slots, emit }) => {
-    const { value } = toRefs(properties)
-    const model = useVModel(properties, 'modelValue', emit, { passive: true })
-    const { attributes } = useSwitch(model, value, properties)
-    return () => h(properties.as, attributes, slots)
+  setup: (properties, { emit, slots, expose }) => {
+    // --- Destructure props.
+    const { as, value, multiple, classActive, onClick } = toRefs(properties)
+
+    // --- Compute states variables.
+    const model = useVModel(properties, 'modelValue')
+    const modelDisabled = useVModel(properties, 'disabled', emit, { passive: true })
+    const modelReadonly = useVModel(properties, 'readonly', emit, { passive: true })
+    const modelLoading = useVModel(properties, 'loading', emit, { passive: true })
+
+    // --- Compute reactive `active` state.
+    const active = computed(() => {
+      if (multiple.value && Array.isArray(model.value))
+        return model.value.includes(value.value)
+      if (multiple.value && !Array.isArray(model.value))
+        return false
+      return model.value === value.value
+    })
+
+    // --- Define `toggle` method.
+    const toggle = async() => {
+      // --- If `multiple`, but value invalid, initialize array.
+      if (multiple.value && !Array.isArray(model.value)) {
+        model.value = [value.value]
+      }
+
+      // --- If `multiple`, add or remove value.
+      else if (multiple.value && Array.isArray(model.value)) {
+        model.value = active.value
+          ? [...model.value].filter(x => x !== value.value)
+          : [...model.value, value.value]
+      }
+
+      // --- If not, set value.
+      else if (typeof value.value === 'boolean') {
+        model.value = !model.value
+      }
+
+      else if (model.value !== value.value) {
+        model.value = value.value
+      }
+
+      // --- Execute `onClick` handler.
+      if (typeof onClick.value === 'function') {
+        modelLoading.value = true
+        await onClick.value(model.value)
+        modelLoading.value = false
+      }
+    }
+
+    const classes = computed(() => (
+      active.value
+        ? classActive.value
+        : undefined
+    ))
+
+    const slotProperties = reactive({
+      active,
+      modelDisabled,
+      modelLoading,
+      modelReadonly,
+      classes,
+      toggle,
+    })
+
+    expose(slotProperties)
+
+    // --- Return virtual DOM node.
+    return () => h(as.value, {
+      // --- State.
+      'disabled': modelDisabled.value || null,
+      'readonly': modelReadonly.value || null,
+
+      // --- Accessibility.
+      'aria-disabled': modelDisabled.value || null,
+      'aria-readonly': modelReadonly.value || null,
+      'aria-busy': modelLoading.value || null,
+
+      // --- Classes.
+      'class': classes.value,
+
+      // --- Events.
+      'onClick': toggle,
+    }, slots.default?.(slotProperties))
   },
 })
