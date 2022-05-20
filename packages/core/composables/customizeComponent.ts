ad5f553f64764e6be974876991db58ee4f92bf16
Stanley Horwood <stanley@hsjm.io>
2022-05-20 06:46:32 +0200

    docs(core): generated doc with gpt3

--- packages/core/composables/customizeComponent.ts
+++ packages/core/composables/customizeComponent.ts
@@ -7,17 +7,23 @@ export type CustomizeComponentPropsFn<T> = (props: Readonly<T>, context: SetupCo
 export type CustomizeComponentSlotFn<T> = (slotProps: Record<string, any>, props: Readonly<T>, context: SetupContext) => Record<string, any>
 
 export type CustomizeComponent = <C extends Component<T>, T = {}>(
-  component?: C,
+  component: C,
   customProps?: CustomizeComponentPropsFn<T> | Record<string, any>,
   customSlots?: CustomizeComponentSlotFn<T> | Record<string, CustomizeComponentSlotFn<T>>,
 ) => C
 
 /**
- * Instantiate a component and defaults it's props and slots.
- * @param component
- * @param customProps
- * @param customSlots
- * @returns
+ * Customizes a component by merging provided props and slots into the component.
+ * @param component The component to customize
+ * @param customProps A mapping of props to merge into the component. If a function, it will receive `props` and `context` and should return an object.
+ * @param customSlots A mapping of slots to merge into the component. If a function, it will receive `slotProps` and `props` and `context` and should return an object.
+ * @returns A new component that merges the provided props and slots into the given component.
+ * @example ```js
+ * import { Switch } from '@headlessui/vue'
+ * import { customizeComponent } from '@hsjm/core'
+ *
+ * const MyCustomizedComponent = customizeComponent(Switch, { class: 'switch' })
+ * ```
  */
 export const customizeComponent: CustomizeComponent = (component: any, customProps: any, customSlots?: any): any =>
   // --- Create and return render function.
