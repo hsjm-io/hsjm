0b979a5d9b8be9b83f4958aabe8f3605fa5c06a5
Stanley Horwood <stanley@hsjm.io>
2022-06-03 11:36:26 +0200

    feat(core): overhauled `Button`

--- packages/core/components/Button.ts
+++ packages/core/components/Button.ts
@@ -1,81 +1,62 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 /* eslint-disable unicorn/no-null */
 import { useVModel } from '@vueuse/core'
-import { PropType, computed, defineComponent, h, resolveComponent, toRefs } from 'vue-demi'
+import { PropType, computed, defineComponent, h, mergeProps, resolveComponent } from 'vue-demi'
 import { RouteLocationRaw } from 'vue-router'
 import { debounce, noop, throttle } from '@hsjm/shared'
+import { exposeToDevtool } from '../composables'
 
-export const Button = defineComponent({
-  name: 'Button',
+export const Button = /* @__PURE__ */ defineComponent({
+  name: 'HsjmButton',
+  inheritAttrs: false,
   props: {
     as: String as PropType<keyof HTMLElementTagNameMap>,
-
-    // --- State.
     disabled: Boolean,
     readonly: Boolean,
     loading: Boolean,
     error: Object as PropType<any>,
-
-    // --- Routing.
-    to: [String, Object] as PropType<RouteLocationRaw | string>,
+    to: [String, Object] as PropType<RouteLocationRaw>,
     replace: Boolean,
     newtab: Boolean,
-
-    // --- Classes.
-    classActive: Object as PropType<any>,
-    classActiveExact: Object as PropType<any>,
-
-    // --- Events.
-    onClick: Function,
-    debounce: Number,
-    throttle: Number,
+    classActive: {} as PropType<unknown>,
+    classActiveExact: {} as PropType<unknown>,
+    onClick: { type: Function, default: noop },
+    debounce: { type: Number, default: 0 },
+    throttle: { type: Number, default: 0 },
   },
-  setup: (properties, { slots, attrs }) => {
-    // --- Destructure props.
-    const {
-      as,
-      to,
-      newtab,
-      classActive,
-      classActiveExact,
-      onClick,
-      throttle: throttleDelay,
-      debounce: debounceDelay,
-    } = toRefs(properties)
-
+  emits: [
+    'update:disabled',
+    'update:readonly',
+    'update:loading',
+    'update:error',
+  ],
+  setup: (properties, { attrs, emit, slots }) => {
     // --- Compute states variables.
-    const modelDisabled = useVModel(properties, 'disabled', undefined, { passive: true })
-    const modelReadonly = useVModel(properties, 'readonly', undefined, { passive: true })
-    const modelLoading = useVModel(properties, 'loading', undefined, { passive: true })
-    const modelError = useVModel(properties, 'error', undefined, { passive: true })
+    const modelDisabled = useVModel(properties, 'disabled', emit, { passive: true })
+    const modelReadonly = useVModel(properties, 'readonly', emit, { passive: true })
+    const modelLoading = useVModel(properties, 'loading', emit, { passive: true })
+    const modelError = useVModel(properties, 'error', emit, { passive: true })
 
     // --- Compute routing variables.
-    const isLink = computed<boolean>(() => to.value === true)
-    const isExternalLink = computed(() => isLink.value && typeof to.value === 'string' && !to.value?.startsWith('/'))
+    const isLink = computed<boolean>(() => properties.to !== undefined)
+    const isExternalLink = computed(() => isLink.value && typeof properties.to === 'string' && !properties.to?.startsWith('/'))
     const isInternalLink = computed(() => isLink.value && !isExternalLink.value)
-    const linkTo = computed(() => (isLink.value && isInternalLink.value && !newtab.value ? to.value : undefined))
-    const linkTarget = computed(() => (isLink.value && newtab.value ? '_blank' : attrs.target as string))
-    const linkRel = computed(() => (isExternalLink.value && newtab.value ? 'noreferrer' : attrs.rel as string))
-    const linkActiveClass = computed(() => (isInternalLink.value ? classActive.value : undefined))
-    const linkExactActiveClass = computed(() => (isInternalLink.value ? classActiveExact.value : undefined))
+    const linkTarget = computed(() => (isLink.value && properties.newtab ? '_blank' : attrs.target as string))
+    const linkRel = computed(() => (isExternalLink.value && properties.newtab ? 'noreferrer' : attrs.rel as string))
+    const linkActiveClass = computed(() => (isInternalLink.value ? properties.classActive : undefined))
+    const linkExactActiveClass = computed(() => (isInternalLink.value ? properties.classActiveExact : undefined))
 
     // --- Compute component type.
     const is = computed(() => {
-      if (as.value) return as.value
-      if (isInternalLink.value) return resolveComponent('RouterLink')
+      if (isInternalLink.value) return 'RouterLink'
       if (isExternalLink.value) return 'a'
-      return 'button'
+      return properties.as ?? 'button'
     })
 
-    // --- Wrap `onClick`.
+    // --- Wrap function to handle loading state & catch error.
     const onClickWrapped = computed(() => {
-      // --- Make sure `onClick` is a function.
-      const onClickValue = onClick.value
-      if (typeof onClickValue !== 'function') return noop
-
-      // --- Wrap function to trash loading state & catch error.
-      const callback = () => {
-        const result = onClickValue()
+      const onClick = () => {
+        const result = properties.onClick?.()
         if (result instanceof Promise) {
           modelLoading.value = true
           result
@@ -85,31 +66,53 @@ export const Button = defineComponent({
       }
 
       // --- Return wrapped function.
-      if (throttleDelay.value) return throttle(callback, throttleDelay.value)
-      if (debounceDelay.value) return debounce(callback, debounceDelay.value)
-      return callback
+      if (properties.throttle > 0) return throttle(onClick, properties.throttle)
+      if (properties.debounce > 0) return debounce(onClick, properties.debounce)
+      return onClick
+    })
+
+    exposeToDevtool({
+      is,
+      isExternalLink,
+      isInternalLink,
+      isLink,
+      linkActiveClass,
+      linkExactActiveClass,
+      linkRel,
+      linkTarget,
+      modelDisabled,
+      modelError,
+      modelLoading,
+      modelReadonly,
+      onClickWrapped,
     })
 
     // --- Return virtual DOM node.
-    return () => h(is, {
-      // --- State.
-      'disabled': modelDisabled.value ?? null,
-      'readonly': modelReadonly.value ?? null,
+    return () => h(
 
-      // --- Accessibility.
-      'aria-disabled': modelDisabled.value ?? null,
-      'aria-readonly': modelReadonly.value ?? null,
-      'aria-busy': modelLoading.value ?? null,
+      // --- VNode type.
+      /^[A-Z]/.test(is.value)
+        ? resolveComponent(is.value) as any
+        : is.value,
 
-      // --- Routing.
-      'to': linkTo,
-      'target': linkTarget,
-      'rel': linkRel,
-      'active-class': linkActiveClass,
-      'exact-active-class': linkExactActiveClass,
+      // --- VNode props.
+      mergeProps(attrs, {
+        'disabled': modelDisabled.value || null,
+        'readonly': modelReadonly.value || null,
+        'aria-disabled': modelDisabled.value || null,
+        'aria-readonly': modelReadonly.value || null,
+        'aria-busy': modelLoading.value || null,
+        'to': isInternalLink.value ? properties.to : undefined,
+        'href': isLink.value ? properties.to : undefined,
+        'target': linkTarget.value,
+        'rel': linkRel.value,
+        'active-class': linkActiveClass.value,
+        'exact-active-class': linkExactActiveClass.value,
+        'onClick': onClickWrapped.value,
+      }),
 
-      // --- Events.
-      'onClick': onClickWrapped,
-    }, slots)
+      // --- VNode slots.
+      slots,
+    )
   },
 })
