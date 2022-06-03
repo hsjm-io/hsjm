2a43f7a5e3ee8f2e5da3b3348e9775c2cf60043a
Stanley Horwood <stanley@hsjm.io>
2022-06-03 11:36:51 +0200

    feat(core): introduced `exposeToDevtools`

---
+++ packages/core/composables/exposeToDevtool.ts
@@ -0,0 +1,17 @@
+import { isProduction } from '@hsjm/shared'
+import { getCurrentInstance, nextTick, reactive } from 'vue-demi'
+
+/**
+ * Exposes an object to the Vue Devtools.
+ * @param object The object to expose
+ */
+export const exposeToDevtool = async(object: Record<string, any>) => {
+  const instance = getCurrentInstance()
+  if (!instance || isProduction()) return
+
+  // --- Await init.
+  await nextTick()
+
+  // @ts-expect-error: `devtoolsRawSetupState` property not referenced.
+  instance.setupState = reactive(object)
+}
