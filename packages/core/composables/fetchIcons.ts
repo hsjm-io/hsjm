dc32cdafea185d4d6556c7b1ea523ca6037be0ec
Stanley Horwood <stanley@hsjm.io>
2022-06-08 13:10:21 +0200

    fix(core): removed `fetchIcon` caching

--- packages/core/composables/fetchIcons.ts
+++ packages/core/composables/fetchIcons.ts
@@ -3,18 +3,12 @@ import { fullIconData, iconToSVG, replaceIDs } from '@iconify/utils'
 import { defaults } from '@iconify/utils/lib/customisations'
 import { fetchCollection } from './fetchCollections'
 
-// --- Cached data.
-const cachedIcons: Record<string, Promise<string>> = {}
-
 /**
  * Retrieve an icon from a remote source.
  * @param iconName Name of the icon.
+ * @param options Options of the icon.
  */
 export const fetchIcon = (iconName: string, options = {} as IconifyIconCustomisations) => {
-  // --- Get from cache if exists.
-  if (iconName in cachedIcons)
-    return cachedIcons[iconName]
-
   // --- Extract icon from remote collection.
   const collectionName = iconName.split(/:|-/)[0]
   const icon = fetchCollection(collectionName)
@@ -41,5 +35,5 @@ export const fetchIcon = (iconName: string, options = {} as IconifyIconCustomisa
     })
 
   // --- Cache and return icon.
-  return (cachedIcons[iconName] = icon)
+  return icon
 }
