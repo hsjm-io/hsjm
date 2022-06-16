a13b1fbf5fa79d26989ccf3b32bca9759dd42c22
Stanley Horwood <stanley@hsjm.io>
2022-06-16 21:42:34 +0200

    fix(fireworks): fixed static collection

--- packages/fireworks/functions/mock.ts
+++ packages/fireworks/functions/mock.ts
@@ -4,7 +4,7 @@ import { getModuleMock } from './../shared/getModuleMock'
 export const mock = <T>(module: Module) => firebaseFunctions?.https.onCall(
   async() => firebaseAdmin
     ?.firestore()
-    .collection('identity')
+    .collection(module.collection)
     .doc()
     .set(getModuleMock<T>(module),
     ),
