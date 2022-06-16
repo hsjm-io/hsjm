24b3d161c93ccdfe01246232946eb716bc99daf0
Stanley Horwood <stanley@hsjm.io>
2022-06-16 21:35:00 +0200

    feat(Button): overhauled `Button` component

--- packages/core/components/Button.ts
+++ packages/core/components/Button.ts
@@ -1,28 +1,45 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 /* eslint-disable unicorn/no-null */
+import { PropType, computed, defineComponent, h, mergeProps } from 'vue-demi'
 import { useVModel } from '@vueuse/core'
-import { PropType, computed, defineComponent, h, mergeProps, resolveComponent } from 'vue-demi'
 import { RouteLocationRaw } from 'vue-router'
 import { debounce, noop, throttle } from '@hsjm/shared'
-import { exposeToDevtool } from '../composables'
+import { IconifyIconCustomisations } from '@iconify/iconify'
+import { exposeToDevtool, resolveComponentType } from '../composables'
+import { Icon } from './Icon'
 
 export const Button = /* @__PURE__ */ defineComponent({
-  name: 'HsjmButton',
+  name: 'Button',
   inheritAttrs: false,
   props: {
-    as: String as PropType<keyof HTMLElementTagNameMap>,
+    as: { type: String as PropType<keyof HTMLElementTagNameMap>, default: 'button' },
+    label: String,
+
+    // --- State.
     disabled: Boolean,
     readonly: Boolean,
     loading: Boolean,
-    error: Object as PropType<any>,
+    error: String,
+
+    // --- Icon
+    icon: String,
+    iconAppend: String,
+    iconPrepend: String,
+    iconOptions: Object as PropType<IconifyIconCustomisations>,
+
+    // --- Routing
     to: [String, Object] as PropType<RouteLocationRaw>,
     replace: Boolean,
     newtab: Boolean,
-    classActive: {} as PropType<unknown>,
-    classActiveExact: {} as PropType<unknown>,
+
+    // --- Interaction
     onClick: { type: Function, default: noop },
     debounce: { type: Number, default: 0 },
     throttle: { type: Number, default: 0 },
+
+    // --- Classes.
+    classActive: {} as PropType<unknown>,
+    classActiveExact: {} as PropType<unknown>,
   },
   emits: [
     'update:disabled',
@@ -30,33 +47,29 @@ export const Button = /* @__PURE__ */ defineComponent({
     'update:loading',
     'update:error',
   ],
-  setup: (properties, { attrs, emit, slots }) => {
+  setup: (props, { attrs, emit, slots }) => {
     // --- Compute states variables.
-    const modelDisabled = useVModel(properties, 'disabled', emit, { passive: true })
-    const modelReadonly = useVModel(properties, 'readonly', emit, { passive: true })
-    const modelLoading = useVModel(properties, 'loading', emit, { passive: true })
-    const modelError = useVModel(properties, 'error', emit, { passive: true })
+    const modelDisabled = useVModel(props, 'disabled', emit, { passive: true })
+    const modelReadonly = useVModel(props, 'readonly', emit, { passive: true })
+    const modelLoading = useVModel(props, 'loading', emit, { passive: true })
+    const modelError = useVModel(props, 'error', emit, { passive: true })
 
     // --- Compute routing variables.
-    const isLink = computed<boolean>(() => properties.to !== undefined)
-    const isExternalLink = computed(() => isLink.value && typeof properties.to === 'string' && !properties.to?.startsWith('/'))
+    const isLink = computed(() => props.to !== undefined)
+    const isExternalLink = computed(() => isLink.value && typeof props.to === 'string' && !props.to?.startsWith('/'))
     const isInternalLink = computed(() => isLink.value && !isExternalLink.value)
-    const linkTarget = computed(() => (isLink.value && properties.newtab ? '_blank' : attrs.target as string))
-    const linkRel = computed(() => (isExternalLink.value && properties.newtab ? 'noreferrer' : attrs.rel as string))
-    const linkActiveClass = computed(() => (isInternalLink.value ? properties.classActive : undefined))
-    const linkExactActiveClass = computed(() => (isInternalLink.value ? properties.classActiveExact : undefined))
 
     // --- Compute component type.
     const is = computed(() => {
       if (isInternalLink.value) return 'RouterLink'
       if (isExternalLink.value) return 'a'
-      return properties.as ?? 'button'
+      return props.as
     })
 
     // --- Wrap function to handle loading state & catch error.
     const onClickWrapped = computed(() => {
       const onClick = () => {
-        const result = properties.onClick?.()
+        const result = props.onClick?.()
         if (result instanceof Promise) {
           modelLoading.value = true
           result
@@ -66,35 +79,34 @@ export const Button = /* @__PURE__ */ defineComponent({
       }
 
       // --- Return wrapped function.
-      if (properties.throttle > 0) return throttle(onClick, properties.throttle)
-      if (properties.debounce > 0) return debounce(onClick, properties.debounce)
+      if (props.throttle > 0) return throttle(onClick, props.throttle)
+      if (props.debounce > 0) return debounce(onClick, props.debounce)
       return onClick
     })
 
-    // --- Expose for debugging.
-    exposeToDevtool({
+    const exposed = {
       is,
       isExternalLink,
       isInternalLink,
       isLink,
-      linkActiveClass,
-      linkExactActiveClass,
-      linkRel,
-      linkTarget,
       modelDisabled,
       modelError,
       modelLoading,
       modelReadonly,
       onClickWrapped,
-    })
+    }
+
+    // --- Expose for debugging.
+    exposeToDevtool(exposed)
+
+    // --- Declare render icon method.
+    const createIconVNode = (icon?: string) => (icon ? h(Icon, { icon, options: props.iconOptions }) : undefined)
 
     // --- Return virtual DOM node.
     return () => h(
 
       // --- VNode type.
-      /^[A-Z]/.test(is.value)
-        ? resolveComponent(is.value) as any
-        : is.value,
+      resolveComponentType<any>(is.value),
 
       // --- VNode props.
       mergeProps(attrs, {
@@ -103,17 +115,39 @@ export const Button = /* @__PURE__ */ defineComponent({
         'aria-disabled': modelDisabled.value || null,
         'aria-readonly': modelReadonly.value || null,
         'aria-busy': modelLoading.value || null,
-        'to': isInternalLink.value ? properties.to : undefined,
-        'href': isLink.value ? properties.to : undefined,
-        'target': linkTarget.value,
-        'rel': linkRel.value,
-        'active-class': linkActiveClass.value,
-        'exact-active-class': linkExactActiveClass.value,
+        'aria-labelledby': props.label,
         'onClick': onClickWrapped.value,
+
+        ...(isInternalLink.value
+          ? {
+            'to': props.to,
+            'active-class': props.classActive,
+            'exact-active-class': props.classActiveExact,
+          }
+          : {}),
+
+        ...(isExternalLink.value
+          ? {
+            href: props.to,
+            rel: props.newtab ? 'noreferrer' : attrs.rel,
+          }
+          : {}),
+
+        ...(isLink.value
+          ? {
+            target: props.newtab ? '_blank' : attrs.target,
+          }
+          : {}),
       }),
 
       // --- VNode slots.
-      slots,
+      {
+        default: () => [
+          slots.prepend?.(exposed) ?? createIconVNode(props.icon ?? props.iconPrepend),
+          slots.default?.(exposed) ?? (props.label ? h('span', props.label) : undefined),
+          slots.append?.(exposed) ?? createIconVNode(props.iconAppend),
+        ],
+      },
     )
   },
 })
