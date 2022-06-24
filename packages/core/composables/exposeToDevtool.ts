693e3e083597f02ce4bbbb2e5928b1703f408e84
Stanley Horwood <stanley@hsjm.io>
2022-06-24 04:06:59 +0200

    feat: converted to sync function

--- packages/core/composables/exposeToDevtool.ts
+++ packages/core/composables/exposeToDevtool.ts
@@ -5,13 +5,14 @@ import { getCurrentInstance, nextTick, reactive } from 'vue-demi'
  * Exposes an object to the Vue Devtools.
  * @param object The object to expose
  */
-export const exposeToDevtool = async(object: Record<string, any>) => {
+export const exposeToDevtool = <T extends Record<string, any>>(object: T): T => {
+  // --- Get current component instance.
   const instance = getCurrentInstance()
-  if (!instance || isProduction) return
+  if (!instance || isProduction) return object
 
-  // --- Await init.
-  await nextTick()
+  // @ts-expect-error: ignore --- Update `setupState` on next tick.
+  nextTick().then(() => instance.setupState = reactive(object))
 
-  // @ts-expect-error: `devtoolsRawSetupState` property not referenced.
-  instance.setupState = reactive(object)
+  // --- Return the object.
+  return object
 }
