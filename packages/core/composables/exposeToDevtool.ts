649723636281173ea8ddc29e0c8588475d10a7fd
Stanley Horwood <stanley@hsjm.io>
2022-06-25 06:25:32 +0200

    feat(core): added `componentInstance` parameter

--- packages/core/composables/exposeToDevtool.ts
+++ packages/core/composables/exposeToDevtool.ts
@@ -1,17 +1,19 @@
 import { isProduction } from '@hsjm/shared'
-import { getCurrentInstance, nextTick } from 'vue-demi'
+import { ComponentInternalInstance, getCurrentInstance, nextTick } from 'vue-demi'
 
 /**
  * Exposes an object to the Vue Devtools.
- * @param object The object to expose
+ * @param {Record<string, any>} object The object to expose
+ * @param {ComponentInternalInstance} [componentInstance] The component instance to expose to
+ * @returns {Record<string, any>} The exposed object
  */
-export const exposeToDevtool = <T extends Record<string, any>>(object: T): T => {
+export const exposeToDevtool = <T extends Record<string, any>>(object: T, componentInstance?: ComponentInternalInstance | null): T => {
   // --- Get current component instance.
-  const instance = getCurrentInstance()
+  const instance = componentInstance ?? getCurrentInstance()
   if (!instance || isProduction) return object
 
   // @ts-expect-error: ignore --- Update `setupState` on next tick.
-  nextTick().then(() => instance.setupState = object)
+  nextTick(() => instance.setupState = object)
 
   // --- Return the object.
   return object
