7114a253ddfcc78d0f04bfa55cbaa5a106c820d7
Stanley Horwood <stanley@hsjm.io>
2022-06-25 05:51:16 +0200

    feat(core)!: passed by reference instead of reactive

--- packages/core/composables/exposeToDevtool.ts
+++ packages/core/composables/exposeToDevtool.ts
@@ -1,5 +1,5 @@
 import { isProduction } from '@hsjm/shared'
-import { getCurrentInstance, nextTick, reactive } from 'vue-demi'
+import { getCurrentInstance, nextTick } from 'vue-demi'
 
 /**
  * Exposes an object to the Vue Devtools.
@@ -11,7 +11,7 @@ export const exposeToDevtool = <T extends Record<string, any>>(object: T): T => 
   if (!instance || isProduction) return object
 
   // @ts-expect-error: ignore --- Update `setupState` on next tick.
-  nextTick().then(() => instance.setupState = reactive(object))
+  nextTick().then(() => instance.setupState = object)
 
   // --- Return the object.
   return object
