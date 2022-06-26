54fccc535681431cbb8418e7e69de4eed7aaeb6f
Stanley Horwood <stanley@hsjm.io>
2022-06-26 17:53:52 +0200

    feat(firebase): added `utils`  exports

---
+++ packages/firebase/utils/index.ts
@@ -0,0 +1,7 @@
+export * from './createQuery'
+export * from './createSharedFirestore'
+export * from './defaultConverter'
+export * from './erase'
+export * from './get'
+export * from './getSnapshotData'
+export * from './save'
