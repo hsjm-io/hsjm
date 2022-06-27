f953526e6697c5a841dece2d1fd9d72c78d04423
Stanley Horwood <stanley@hsjm.io>
2022-06-27 03:01:23 +0200

    feat(fireworks)!: refactored validators

--- packages/fireworks/shared/validators.ts
+++ packages/fireworks/shared/validators.ts
@@ -1,19 +1,28 @@
 import { FirebaseContext } from './types'
 
-export const isUserId = (id: string, _: any, context: FirebaseContext) =>
-  context.admin.auth().getUser(id).then(user => !!user)
+export const isUserId = (id: string, _: any, { admin }: FirebaseContext) => admin
+  .auth()
+  .getUser(id)
+  .then(user => !!user)
 
-export const toFirestoreIdentity = (id: string, _: any, context: FirebaseContext) =>
-  context.admin.firestore().collection('identity').where('userId', '==', id).get()
-    .then(snapshot => snapshot.docs[0].ref)
+export const toFirestoreIdentity = (id: string, _: any, { admin }: FirebaseContext) => admin
+  .firestore()
+  .collection('identity')
+  .where('userId', '==', id)
+  .get()
+  .then(snapshot => snapshot.docs[0].ref)
 
-export const toFirestoreReference = (id: string, path: string, context: FirebaseContext) =>
-  context.admin.firestore().collection(path).doc(id)
+export const toFirestoreReference = (id: string, path: string, { admin }: FirebaseContext) => admin
+  .firestore()
+  .collection(path)
+  .doc(id)
 
-export const toFirestoreReferenceArray = (ids: string[], path: string, context: FirebaseContext) => {
-  const firestore = context.admin.firestore()
-  return firestore ? ids.map(id => firestore.collection(path).doc(id)) : []
-}
+export const toFirestoreReferenceArray = (ids: string[], path: string, context: FirebaseContext) =>
+  ids.map(id => toFirestoreReference(id, path, context))
 
-export const documentExists = async(id: string, path: string, context: FirebaseContext) =>
-  context.admin.firestore().collection(path).doc(id).get().then(snapshot => snapshot.exists)
+export const isFirestoreReference = (id: string, path: string, { admin }: FirebaseContext) => admin
+  .firestore()
+  .collection(path)
+  .doc(id)
+  .get()
+  .then(snapshot => snapshot.exists)
