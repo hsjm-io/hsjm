e04a2de930f15b259300b3f1ad4ef73473cd76d4
Stanley Horwood <stanley@hsjm.io>
2022-06-30 08:31:44 +0200

    feat(fireworks): refactored `mergeModules` to allow rules overwrites

--- packages/fireworks/shared/mergeModules.test.ts
+++ packages/fireworks/shared/mergeModules.test.ts
@@ -1,6 +1,9 @@
 import { expect, it } from 'vitest'
 import { mergeModules } from './mergeModules'
 
+const isString = (v: any) => typeof v === 'string'
+const isNumber = (v: any) => typeof v === 'number'
+
 it('should merge modules', () => {
   const module1 = {
     path: 'module1',
@@ -8,12 +11,12 @@ it('should merge modules', () => {
       field1: {
         name: 'Field 1',
         group: 'group1',
-        rules: [],
+        rules: [isString],
       },
       field2: {
         name: 'Field 2',
         group: 'group1',
-        rules: [],
+        rules: [isString],
       },
     },
   }
@@ -23,49 +26,29 @@ it('should merge modules', () => {
       field1: {
         name: 'Field 1',
         group: 'group1',
-        rules: [],
-      },
-      field3: {
-        name: 'Field 3',
-        group: 'group2',
-        rules: [],
+        rules: [isNumber],
       },
     },
   }
   const module3 = {
     path: 'module3',
     fields: {
-      field1: {
-        name: 'Field 1',
-        group: 'group1',
-        rules: [],
-      },
       field3: {
         name: 'Field 3',
         group: 'group2',
-        rules: [],
+        rules: [isNumber],
       },
     },
   }
   const mergedModule = mergeModules(module1, module2, module3)
-  expect(mergedModule).toEqual({
-    path: 'module3',
-    fields: {
-      field1: {
-        name: 'Field 1',
-        group: 'group1',
-        rules: [],
-      },
-      field2: {
-        name: 'Field 2',
-        group: 'group1',
-        rules: [],
-      },
-      field3: {
-        name: 'Field 3',
-        group: 'group2',
-        rules: [],
-      },
-    },
-  })
+  expect(mergedModule.path).toEqual('module3')
+  expect(mergedModule.fields?.field1.name).toEqual('Field 1')
+  expect(mergedModule.fields?.field1.group).toEqual('group1')
+  expect(mergedModule.fields?.field1.rules).toEqual([isNumber])
+  expect(mergedModule.fields?.field2.name).toEqual('Field 2')
+  expect(mergedModule.fields?.field2.group).toEqual('group1')
+  expect(mergedModule.fields?.field2.rules).toEqual([isString])
+  expect(mergedModule.fields?.field3.name).toEqual('Field 3')
+  expect(mergedModule.fields?.field3.group).toEqual('group2')
+  expect(mergedModule.fields?.field3.rules).toEqual([isNumber])
 })
