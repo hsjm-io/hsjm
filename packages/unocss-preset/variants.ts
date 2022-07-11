41203097b72352dc02036fe836c8e375fdd78957
Stanley Horwood <stanley@hsjm.io>
2022-07-11 17:21:22 +0200

    feat(unocss-preset): introduced new variant `selected`

--- packages/unocss-preset/variants.ts
+++ packages/unocss-preset/variants.ts
@@ -1,34 +1,32 @@
 import { Variant } from 'unocss'
 
-/** Variant for selector `&[aria-current="page"]`. */
-export const variantCurrentPage: Variant = {
+const createVariant = (keyword: string, query: string | string[]) => ({
   match: (input: string) => {
-    const match = input.match(/^current[:-]/)
+    const regexp = new RegExp(`^${keyword}[:-]`)
+    const match = input.match(regexp)
+    const queries = Array.isArray(query) ? query : [query]
     if (match) {
       return {
         matcher: input.slice(match[0].length),
-        selector: (s: string) => `${s}[aria-current="page"]`,
+        selector: (s: string) => queries.map(query => `${s}${query}`).join(','),
       }
     }
   },
-  autocomplete: '(current):',
-}
+  autocomplete: `(${keyword}):`,
+})
+
+/** Variant for selector `&[aria-current="page"]`. */
+export const variantCurrentPage: Variant = createVariant('current', '[aria-current="page"]')
 
 /** Variant for selector `&[aria-busy="true"]`. */
-export const variantLoading: Variant = {
-  match: (input: string) => {
-    const match = input.match(/^loading[:-]/)
-    if (match) {
-      return {
-        matcher: input.slice(match[0].length),
-        selector: (s: string) => `${s}[aria-busy="true"]`,
-      }
-    }
-  },
-  autocomplete: '(loading):',
-}
+export const variantLoading: Variant = createVariant('loading', '[aria-busy="true"]')
 
+/** Variant for selected &[selected="true"] */
+export const variantSelected: Variant = createVariant('selected', ['[selected="true"]', '[aria-selected="true"]'])
+
+/** All variants. */
 export const variants: Variant[] = [
   variantCurrentPage,
+  variantSelected,
   variantLoading,
 ]
