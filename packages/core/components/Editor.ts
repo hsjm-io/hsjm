3961d67b05d4a5e2eab46196139b2eace9cb8ea1
Stanley Horwood <stanley@hsjm.io>
2022-07-13 23:50:31 +0200

    feat(core)!: added two-way binding feature

--- packages/core/components/Editor.ts
+++ packages/core/components/Editor.ts
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
-import { PropType, defineComponent, h, mergeProps, ref } from 'vue-demi'
+import { PropType, defineComponent, h, mergeProps, ref, watch } from 'vue-demi'
 import { useVModel } from '@vueuse/core'
 import { tryOnMounted } from '@vueuse/shared'
 import { isBrowser } from '@hsjm/shared'
-import { /* @__PURE__ */ EditorView, basicSetup } from 'codemirror'
-import { /* @__PURE__ */ EditorViewConfig } from '@codemirror/view'
-import { /* @__PURE__ */ oneDark } from '@codemirror/theme-one-dark'
-import { /* @__PURE__ */ markdown } from '@codemirror/lang-markdown'
+import { EditorView } from 'codemirror'
+import { EditorViewConfig } from '@codemirror/view'
+import { exposeToDevtool } from '../utils'
 
 export const Editor = /* @__PURE__ */ defineComponent({
   name: 'Editor',
@@ -16,6 +15,8 @@ export const Editor = /* @__PURE__ */ defineComponent({
     options: { type: Object as PropType<EditorViewConfig>, default: () => ({}) },
   },
   emit: [
+    'emit',
+    'blur',
     'onUpdate:modelValue',
   ],
   setup(props, { attrs, emit }) {
@@ -24,13 +25,38 @@ export const Editor = /* @__PURE__ */ defineComponent({
     const editor = ref<EditorView>()
 
     // --- Declare function to instantiate the editor.
-    const createEditor = () =>
-      editor.value = new EditorView({
-        doc: modelValue.value,
-        extensions: [basicSetup, markdown(), oneDark],
-        parent: editorElement.value,
-        ...props.options,
-      })
+    const createEditor = () => editor.value = new EditorView({
+      doc: modelValue.value,
+      parent: editorElement.value,
+      ...props.options,
+      extensions: [
+        props.options.extensions ?? [],
+        EditorView.updateListener.of((viewUpdate) => {
+          if (viewUpdate.docChanged) modelValue.value = viewUpdate.state.doc.toString()
+          if (viewUpdate.focusChanged) emit(viewUpdate.view.hasFocus ? 'focus' : 'blur', viewUpdate)
+        }),
+      ],
+    })
+
+    // --- Expose the contentMarkdown to the devtool.
+    exposeToDevtool({
+      modelValue,
+      editorElement,
+      editor,
+    })
+
+    // --- Watch for external changes to the modelValue.
+    watch(modelValue, (value) => {
+      if (editor.value && !editor.value?.hasFocus) {
+        editor.value.dispatch({
+          changes: {
+            from: 0,
+            to: editor.value.state.doc.length,
+            insert: value,
+          },
+        })
+      }
+    })
 
     // --- Create the editor on mount.
     if (isBrowser) tryOnMounted(createEditor)
