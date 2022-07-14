20f4a2c5aac1315e8bb33b7847af7a867dee8f9d
Stanley Horwood <stanley@hsjm.io>
2022-07-14 17:11:44 +0200

    feat(unocss-preset)!: renamed `invalid` variant to `error`

--- packages/unocss-preset/variants.ts
+++ packages/unocss-preset/variants.ts
@@ -1,6 +1,7 @@
-import { Variant } from 'unocss'
+import { Variant, VariantObject } from 'unocss'
 
-const createVariant = (keyword: string, query: string | string[]) => ({
+const createVariant = (keyword: string, query: string | string[]): VariantObject => ({
+  name: keyword,
   match: (input: string) => {
     const regexp = new RegExp(`^${keyword}[:-]`)
     const match = input.match(regexp)
@@ -8,24 +9,24 @@ const createVariant = (keyword: string, query: string | string[]) => ({
     if (match) {
       return {
         matcher: input.slice(match[0].length),
-        selector: (s: string) => queries.map(query => `${s}${query}`).join(','),
+        selector: (s: string) => queries.map(query => query.replace(/&/g, s)).join(','),
       }
     }
   },
-  autocomplete: `(${keyword}):`,
+  autocomplete: `${keyword}:`,
 })
 
 /** Variant for selector `&[aria-current="page"]`. */
-export const variantCurrentPage: Variant = createVariant('current', '[aria-current="page"]')
+export const variantCurrentPage: Variant = createVariant('current', '&[aria-current="page"]')
 
 /** Variant for selected &[selected="true"] */
-export const variantSelected: Variant = createVariant('selected', ['[selected="true"]', '[aria-selected="true"]'])
+export const variantSelected: Variant = createVariant('selected', ['&[selected="true"]', '&[aria-selected="true"]'])
 
 /** Variant for selected &[aria-invalid="true"] */
-export const variantInvalid: Variant = createVariant('invalid', '[aria-invalid="true"]')
+export const variantInvalid: Variant = createVariant('error', ['&[aria-invalid="true"]', '*[aria-invalid="true"] &'])
 
 /** Variant for selector `&[aria-busy="true"]`. */
-export const variantLoading: Variant = createVariant('loading', '[aria-busy="true"]')
+export const variantLoading: Variant = createVariant('loading', '&[aria-busy="true"]')
 
 /** All variants. */
 export const variants: Variant[] = [
