87c2595b8ab370aa4ecb74c0a9ba5dd4e2121b28
Stanley Horwood <stanley@hsjm.io>
2022-07-15 10:21:24 +0200

    chore: overwritten `pickFirst` to always be true

--- packages/fireworks/composables/useUserIdentity.ts
+++ packages/fireworks/composables/useUserIdentity.ts
@@ -5,14 +5,14 @@ import { UseFirestoreOptions, useFirestore } from './useFirestore'
 import { useAuth } from './useAuth'
 
 /** Shared composable for current user's `Identity` documents. */
-export const useUserIdentity = /* @__PURE__ */ createSharedComposable((options: UseFirestoreOptions<Identity>) => {
+export const useUserIdentity = /* @__PURE__ */ createSharedComposable((options: UseFirestoreOptions<Identity> = {}) => {
   const { user } = useAuth()
   const query = reactive({ userId: computed(() => user.value?.uid) })
   return useFirestore<Identity>(identityModule.path, query, {
     sync: true,
-    pickFirst: true,
     keepAlive: true,
     initialValue: query,
     ...options,
+    pickFirst: true,
   })
 })
